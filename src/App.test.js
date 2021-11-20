f0d33e15c11f8928715197f8ecebafedd3e3aff4
Patrick Obafemi <eloquentintech@gmail.com>
2021-11-20 09:25:57 +0100

    First Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme';
+import Counter from './Counter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Counter testing', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Counter />)
+  });
+
+  test('renders the title of counter', () => {
+    expect(wrapper.find("h1").text()).toContain('This is a counter app');
+  });
+
+  test('renders the button text', () => {
+      expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test('renders the counter state value in a div', () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  // test("clicks on button and decrements the value of counter", () => {
+  //     wrapper.find("#decrement-btn").simulate("click");
+  //     expect(wrapper.find("#counter-value").text()).toBe("0");
+  // }); 
+
+  test("clicks on button and decrements the value of counter but never gets 0", () => {
+
+    wrapper.find("#increment-btn").simulate("click");
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+}); 
+  
+  test("clicks on button and increments the value of counter", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+}); 
+
 });
---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+function Counter() {
+
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+           <h1>This is a counter app</h1>
+        <div id="counter-value">{counter}</div>
+        <button id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
+        <button id="decrement-btn" onClick={() => setCounter(counter - 1)}  disabled={counter === 0}>Decrement</button>
+     
+        </div>
+    )
+}
+
+export default Counter
